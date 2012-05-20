cf12ed05ddb33fed78ee3c0241f3f2a9919e953f
Jeremy Miller <jeremydmiller@yahoo.com>
2012-05-20 11:43:29 -0500

    initial commit

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyDescription("0.9.8.51136")]
+[assembly: AssemblyProduct("FubuMVC.Deployers")]
+[assembly: AssemblyCopyright("Copyright 2011-2012 Dru Sellers, Jeremy D. Miller, et al. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("0.9.8.0")]
+[assembly: AssemblyFileVersion("0.9.8.51136")]
+
+[assembly: AssemblyInformationalVersion("0.9.8.0")]
+
